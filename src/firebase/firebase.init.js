86b751bfc6a1cebc090e40cdb2ab003941b8a941
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-14 01:55:25 +0600

    Form create and Sign in and up email and password

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCUpp7y_UlPzGacXcSizmrPlKwVa-MYgY4",
+    authDomain: "email-password-auth-798e6.firebaseapp.com",
+    projectId: "email-password-auth-798e6",
+    storageBucket: "email-password-auth-798e6.appspot.com",
+    messagingSenderId: "396200623723",
+    appId: "1:396200623723:web:28c0a04ddda78d920a3b2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
