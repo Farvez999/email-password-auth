6a03f9f11af317c99d941abee542f3d77d47ccb7
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-15 00:01:04 +0600

    Valided login with email and password

--- src/components/BootstrapRegister.js
+++ src/components/BootstrapRegister.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
+const auth = getAuth();
+
 const BootstrapRegister = () => {
+    const [passwordError, setPasswordError] = useState('');
+    const [registerSuccess, setRegisterSuccess] = useState(false)
+
     const handleSubmitRegister = (event) => {
         event.preventDefault();
+        setRegisterSuccess(false)
+        const form = event.target;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password)
+        console.log(email, password);
+
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setPasswordError('Please provide at least one uppercase')
+            return;
+        }
+        if (password.length < 6) {
+            setPasswordError('Please password should be 6 characters')
+            return;
+        }
+        if (!/(?=.*[!@#$%^&*])/.test(password)) {
+            setPasswordError('Please least one special character')
+            return;
+        }
+        setPasswordError('')
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                setRegisterSuccess(true)
+                form.reset()
+            })
+            .catch(error => {
+                console.error('error', error)
+                setPasswordError(error.message)
+            })
     }
 
     return (
@@ -14,19 +48,19 @@ const BootstrapRegister = () => {
             <Form onSubmit={handleSubmitRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+
+                <p className='text-danger'>{passwordError}</p>
+                {
+                    registerSuccess && <p className='text-success'>User Create Successful</p>
+                }
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
